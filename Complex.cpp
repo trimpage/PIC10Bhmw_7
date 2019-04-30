03abf32fedd976d769adb965cf18a12c17a4d066
trimpage <trimpage1234@yahoo.com>
2019-04-30 03:14:16 -0700

    a

---
+++ Complex.cpp
@@ -0,0 +1,9 @@
+//Complex.cpp
+
+#include "Complex.h"
+
+//COMPLEX CLASS DEFINITIONS
+//constructors
+Complex::Complex(const double& _real = 0, const double& _imaginary = 0) : real(_real), imaginary(_imaginary) {};
+Complex::Complex(const std::string& number) {}
+
